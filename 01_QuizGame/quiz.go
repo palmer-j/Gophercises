b093d5296b988748d9f436a80e35344d8bca0525
Johnny Palmer <johnny.palmer.work@gmail.com>
2023-02-09 23:50:44 +0000

    read csv filenmae from flag

---
+++ 01_QuizGame/quiz.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	)
+
+func main(){
+
+	csvFilename := flag.String("csv", "problems.csv", "filepath to problems csv, expects format 'problem,answer'")
+
+	fmt.Println(*csvFilename)
+
+	_, err := os.Open(*csvFilename)
+
+	if err != nil {
+		exit(fmt.Sprintf("failed to read file %s", csvFilename))
+	}
+}
+
+type problem struct{
+	problem string
+	answer string
+}
+
+// func getProblems(filename) [][] string{
+// 	res = make()
+// }
+
+func exit(msg string){
+	fmt.Println(msg)
+	os.Exit(1)
+}
